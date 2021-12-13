f6ae5d2fbbb470f2e5d73f67934ba6ce3bc921a5
ulvi1993 <ulvi.esedzadeh@gmail.com>
2021-12-13 18:22:56 +1300

    Blog and Blog Detail Page Created

---
+++ Ulvino/Ulvino/Controllers/BlogController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ulvino.Models;
+
+namespace Ulvino.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Blog> blogs = _context.Blogs.ToList();
+
+            return View(blogs);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            if (!ModelState.IsValid) return View();
+
+            Blog blog = _context.Blogs.FirstOrDefault(x => x.Id == id);
+
+            return View(blog);
+        }
+    }
+}
