616e4f44efbfa9e2b6da40aaab957977da7af10f
ulvi1993 <ulvi.esedzadeh@gmail.com>
2021-12-13 17:30:44 +1300

    FAQ Page Created

---
+++ Ulvino/Ulvino/Controllers/FaqController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ulvino.Models;
+
+namespace Ulvino.Controllers
+{
+    public class FaqController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public FaqController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Faq> faqs = _context.Faqs.ToList();
+
+            return View(faqs);
+        }
+    }
+}
