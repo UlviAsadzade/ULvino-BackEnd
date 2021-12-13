c8a1bba802cc1bb7d260c04a389f625336877562
ulvi1993 <ulvi.esedzadeh@gmail.com>
2021-12-13 18:34:41 +1300

    Contact Page Created

---
+++ Ulvino/Ulvino/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ulvino.Models;
+
+namespace Ulvino.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ContactController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            Setting setting = _context.Settings.FirstOrDefault();
+
+            return View(setting);
+        }
+    }
+}
