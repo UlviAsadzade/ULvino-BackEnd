761ac9efb4bb967425d940375e6286a58037b947
ulvi1993 <ulvi.esedzadeh@gmail.com>
2021-12-15 20:59:03 +1300

    Error Page Created

---
+++ Ulvino/Ulvino/Models/Color.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ulvino.Models
+{
+    public class Color
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 100)]
+        public string Name { get; set; }
+
+        public List<Product> Products { get; set; }
+
+    }
+}
